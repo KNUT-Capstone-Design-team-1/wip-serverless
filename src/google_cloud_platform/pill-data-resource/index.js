d4f471985549fcfcb14282732a5bcf7d0905dfde
YongSiHun <tlgns1537@naver.com>
2026-01-10 13:41:26 +0900

    feat: pill_data 테이블 리소스 API 구현

---
+++ src/google_cloud_platform/pill-data-resource/index.js
@@ -0,0 +1,56 @@
+const functions = require("@google-cloud/functions-framework");
+const { authenticate } = require("./authentication");
+const resourceData = require("./resource.json");
+
+const PAGE_LIMIT = 1000;
+
+/**
+ * pill_data 테이블 원천 데이터 반환
+ * @param {Request} req
+ * @returns
+ */
+function getPillDataResource(req) {
+  const { page } = req.query;
+
+  if (!resources) {
+    return { success: false, message: "Invalid Resource Data" };
+  }
+
+  const total = resource.length;
+  const totalPage = Math.ceil(Number(total) / Number(PAGE_LIMIT));
+  const current = (Number(page) - 1) * Number(PAGE_LIMIT);
+
+  const { resources } = resourceData;
+
+  const resource = resources.slice(
+    Number(current),
+    Number(current) + Number(limit)
+  );
+
+  return { success: true, resource, total, totalPage, current };
+}
+
+functions.http("pill-data-resource", (req, res) => {
+  switch (req.method) {
+    case "GET": {
+      if (!authenticate(req)) {
+        res.sendStatus(401);
+        return;
+      }
+
+      if (!req.query.page) {
+        res.status(402).send(`Page not received`);
+      }
+
+      const result = getPillDataResource(req);
+
+      if (!result.success) {
+        res.status(500).send(result.message);
+        return;
+      }
+
+      res.status(200).json(result);
+      break;
+    }
+  }
+});
