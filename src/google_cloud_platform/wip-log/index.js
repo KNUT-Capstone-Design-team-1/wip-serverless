e7284ce89ec768c35a5750e626bc4c17fd0acb02
YongSiHun <tlgns1537@naver.com>
2026-01-14 22:50:17 +0900

    feat: 로깅 서버 구현

---
+++ src/google_cloud_platform/wip-log/index.js
@@ -0,0 +1,31 @@
+const functions = require("@google-cloud/functions-framework");
+const { authenticate } = require("./authentication");
+
+/**
+ * 로그를 증적
+ * @param {Request} req
+ */
+function writeLog(req) {
+  const { logLevel, logData } = req.body;
+
+  console.log(`==========Log Level: ${logLevel} : ${logData}==========`);
+}
+
+functions.http("wip-log", (req, res) => {
+  switch (req.method) {
+    case "POST": {
+      if (!authenticate(req)) {
+        res.sendStatus(401);
+        return;
+      }
+
+      res.sendStatus(201); // 오류 상황에서 빠른 응답을 위해 응답 후 로그 증적
+
+      writeLog(req).catch((e) =>
+        console.log(`Failed to write log. %s`, e?.stack || e)
+      );
+
+      break;
+    }
+  }
+});
