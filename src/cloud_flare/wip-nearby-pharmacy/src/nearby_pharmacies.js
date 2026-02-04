9721305f743137b108697cc8e974a39d17154cb1
YongSiHun <tlgns1537@naver.com>
2026-02-04 21:38:15 +0900

    refact: 주변 약국 API 리팩토링

--- src/cloud_flare/wip-nearby-pharmacy/src/nearby_pharmacies.js
+++ src/cloud_flare/wip-nearby-pharmacy/src/nearby_pharmacies.js
@@ -1,13 +1,12 @@
 /**
- * 주변 약국 조회
+ * Request로 부터 검색 파라미터를 반환
  * @param {Request} request 요청 객체
- * @param {Env} env workers 환경 객체
- * @returns 
+ * @returns
  */
-export async function readPharmacies(request, env) {
+function getSearchParams(request) {
   const queryParams = new URL(request.url).searchParams;
 
-  const filter = {
+  const searchParams = {
     x: queryParams.get("x"),
     y: queryParams.get("y"),
     states: queryParams.get("states"),
@@ -16,18 +15,39 @@ export async function readPharmacies(request, env) {
     address: queryParams.get("address"),
   };
 
-  const enteredFilterEntries = Object.entries(filter).filter(([_k, v]) => v);
-  if (!enteredFilterEntries.length) {
-    return new Response("No Filter", { status: 400 });
+  return searchParams;
+}
+
+/**
+ * 검색 파라미터를 밸리데이션
+ * @param {Object} searchParams 검색 파라미터
+ * @returns
+ */
+function validateSearchParams(searchParams) {
+  const enteredSearchParamsEntries = Object.entries(searchParams).filter(
+    ([_k, v]) => v,
+  );
+
+  if (!enteredSearchParamsEntries.length) {
+    return { success: false, message: "Invalid Search Params" };
   }
 
-  const { x, y, states, region, district, address } = filter;
+  const { x, y } = searchParams;
 
   if ((x && !y) || (!x && y)) {
-    return new Response("Coordinate data requires both x and y", {
-      status: 400,
-    });
+    return { success: false, message: "Coordinate data requires both x and y" };
   }
+
+  return { success: true, message: "" };
+}
+
+/**
+ * WHERE 쿼리와 파라미터를 생성
+ * @param {Object} searchParams 검색 파라미터
+ * @returns
+ */
+function generateWhere(searchParams) {
+  const { x, y, states, region, district, address } = searchParams;
 
   const filterMapper = {
     states: [`states = ?`, [states]],
@@ -36,44 +56,64 @@ export async function readPharmacies(request, env) {
     address: [`address LIKE ?`, [`%${address}%`]],
   };
 
-  let whereQuery = `WHERE `;
+  const whereClauses = [];
   const whereValues = [];
 
-  const whereFilterEntries = enteredFilterEntries.filter(
-    ([k]) => !["x", "y"].includes(k)
+  const attributeFilters = Object.entries(searchParams).filter(
+    ([k, v]) => v && !["x", "y"].includes(k),
   );
 
-  if (whereFilterEntries.length) {
-    whereQuery += whereFilterEntries
-      .map(([k]) => filterMapper[k][0])
-      .join(" OR ");
+  if (attributeFilters.length) {
+    whereClauses.push(
+      attributeFilters.map(([k]) => filterMapper[k][0]).join(" AND "),
+    );
 
-    whereValues.push(whereFilterEntries.flatMap(([k]) => filterMapper[k][1]));
+    whereValues.push(...attributeFilters.flatMap(([k]) => filterMapper[k][1]));
   }
 
-  if (x && y) {
+  const hasCoordinate = x && y;
+  if (hasCoordinate) {
     const earthRadius = 6371; // km
     const distanceLimit = 3; // km
 
-    const coordinateWhereQuery = `? * acos(
-             cos(radians(?)) * cos(radians(y)) *
-             cos(radians(x) - radians(?)) +
-             sin(radians(?)) * sin(radians(y))
-           ) < ?`;
-
-    whereFilterEntries.length
-
-    whereQuery += whereFilterEntries.length ? ` AND ${coordinateWhereQuery}` : coordinateWhereQuery;
+    whereClauses.push(`? * acos(
+      cos(radians(?)) * cos(radians(y)) *
+      cos(radians(x) - radians(?)) +
+      sin(radians(?)) * sin(radians(y))
+    ) < ?`);
 
     whereValues.push(earthRadius, y, x, y, distanceLimit);
   }
 
+  const whereQuery = whereClauses.length
+    ? `WHERE ${whereClauses.join(" AND ")}`
+    : "";
+
+  return { whereQuery, whereValues };
+}
+
+/**
+ * 주변 약국 조회
+ * @param {Request} request 요청 객체
+ * @param {Object} env workers 환경 객체
+ * @returns
+ */
+export async function readPharmacies(request, env) {
+  const searchParams = getSearchParams(request);
+
+  const validate = validateSearchParams(searchParams);
+  if (!validate.success) {
+    return new Response(validate.message, { status: 400 });
+  }
+
+  const { whereQuery, whereValues } = generateWhere(searchParams);
+
   const sql = `SELECT id, name, states, region, district, postalCode, address, telephone, openData, x, y
                FROM NearbyPharmacies
-               ${whereQuery}`;
+               ${whereQuery || ''}`;
 
   const statement = env.D1.prepare(sql).bind(...whereValues);
-  const pharmacies = await env.D1.batch([statement]);
+  const pharmacies = await statement.all();
 
   return Response.json(pharmacies);
 }
