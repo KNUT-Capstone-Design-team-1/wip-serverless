38515e0468e62f8bb082695f9c52f6b43f35c5cf
YongSiHun <tlgns1537@naver.com>
2026-02-04 23:55:03 +0900

    refact: pill_data 리소스 API gzip 적용

--- src/google_cloud_platform/wip-pill-data-resource/index.js
+++ src/google_cloud_platform/wip-pill-data-resource/index.js
@@ -1,13 +1,22 @@
 const functions = require("@google-cloud/functions-framework");
+const compression = require("compression");
+const express = require("express");
+
 const { authenticate } = require("./authentication");
 const pillData = require("./pill_data.json");
 
 const PAGE_LIMIT = 5000;
 
+const app = express();
+
+app.use(
+  compression({
+    threshold: 1024, // 1KB 이상만 압축
+  }),
+);
+
 /**
  * pill_data 테이블 원천 데이터 반환
- * @param {Request} req
- * @returns
  */
 function getPillDataResource(req) {
   const { page } = req.query;
@@ -17,40 +26,35 @@ function getPillDataResource(req) {
   }
 
   const { resources } = pillData;
-  
-  const total = resources.length;
-  const totalPage = Math.ceil(Number(total) / Number(PAGE_LIMIT));
-  const current = (Number(page) - 1) * Number(PAGE_LIMIT);
 
-  const resource = resources.slice(
-    Number(current),
-    Number(current) + Number(PAGE_LIMIT)
-  );
+  const total = resources.length;
+  const totalPage = Math.ceil(total / PAGE_LIMIT);
+  const current = (Number(page) - 1) * PAGE_LIMIT;
+
+  const resource = resources.slice(current, current + PAGE_LIMIT);
 
   return { success: true, data: { resource, total, totalPage, current } };
 }
 
-functions.http("wip-pill-data-resource", (req, res) => {
-  switch (req.method) {
-    case "GET": {
-      if (!authenticate(req)) {
-        res.sendStatus(401);
-        return;
-      }
-
-      if (!req.query.page) {
-        res.status(402).send(`Page not received`);
-      }
-
-      const result = getPillDataResource(req);
-
-      if (!result.success) {
-        res.status(500).send(result.message);
-        return;
-      }
-
-      res.status(200).json(result.data);
-      break;
-    }
+app.get("/", (req, res) => {
+  if (!authenticate(req)) {
+    res.sendStatus(401);
+    return;
   }
+
+  if (!req.query.page) {
+    res.status(400).send("Page not received");
+    return;
+  }
+
+  const result = getPillDataResource(req);
+
+  if (!result.success) {
+    res.status(500).send(result.message);
+    return;
+  }
+
+  res.status(200).json(result.data);
 });
+
+functions.http("wip-pill-data-resource", app);
