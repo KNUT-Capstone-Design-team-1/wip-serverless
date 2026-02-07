0fc59885b6b603caccbb2f73e4e56fa4ceb93000
YongSiHun <tlgns1537@naver.com>
2026-02-07 22:00:23 +0900

    improve: 데이터베이스 버전 API에서 마크이미지 및 주변약국 데이터베이스의 버전도추가
    - express 도입

--- src/google_cloud_platform/wip-database-version/index.js
+++ src/google_cloud_platform/wip-database-version/index.js
@@ -1,36 +1,43 @@
-const functions = require('@google-cloud/functions-framework');
+const functions = require("@google-cloud/functions-framework");
+const compression = require("compression");
+const express = require("express");
 const { authenticate } = require("./authentication");
 const config = require("./config.json");
 
+const app = express();
+
+app.use(
+  compression({
+    threshold: 1024, // 1KB 이상만 압축
+  }),
+);
+
 /**
  * 데이터베이스 버전을 조회
- * @returns 
+ * @returns
  */
 function getDatabaseVersion() {
   if (!config?.pillData) {
-    return { success: false, message: 'Invalid Config File' };
+    return { success: false, message: "Invalid Config File" };
   }
 
   return { success: true, data: config };
 }
 
-functions.http('wip-database-version', (req, res) => {
-  switch (req.method) {
-    case "GET": {
-      if (!authenticate(req)) {
-        res.sendStatus(401);
-        return;
-      }
-
-      const result = getDatabaseVersion(req);
-
-      if (!result.success) {
-        res.status(500).send(result.message);
-        return;
-      }
-
-      res.status(200).json(result.data);
-      break;
-    }
+app.get("/", (req, res) => {
+  if (!authenticate(req)) {
+    res.sendStatus(401);
+    return;
   }
+
+  const result = getDatabaseVersion();
+
+  if (!result.success) {
+    res.status(500).send(result.message);
+    return;
+  }
+
+  res.status(200).json(result.data);
 });
+
+functions.http("wip-database-version", app);
